29ce8d49e9e7f7e6e78491c3b023756b460445f5
Enriek Naldo Patera <Pateraenriek@gmail.com>
2024-07-07 17:14:11 +0700

    make homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import './globals.css';
 
 import { cn } from '@/lib/utils';
-import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ThemeProvider } from '@/components/theme-provider';
 
 const fontSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: '--font-sans'
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans',
 });
 
 export const metadata: Metadata = {
-  title: "CarePulse",
-  description: "A healthcare management systek",
+  title: 'CarePulse',
+  description: 'A healthcare management systek',
 };
 
 export default function RootLayout({
@@ -22,12 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-        >
+    <html lang='en'>
+      <body
+        className={cn(
+          'min-h-screen bg-dark-300 font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute='class' defaultTheme='dark'>
           {children}
         </ThemeProvider>
       </body>
